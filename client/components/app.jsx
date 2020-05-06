47af7382a9aed6f679f07e9f15724b87b24d0666
David Lyu <lyu.david@yahoo.com>
2020-05-06 11:26:20 -0700

    create the functionality for the View Destination page along with create routers in App and create the destinationItem/List components

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import DestinationList from './destination-list';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -9,17 +14,34 @@ export default class App extends React.Component {
     };
   }
 
-  componentDidMount() {
-    fetch('/api/health-check')
-      .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
-  }
-
   render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{this.state.message.toUpperCase()}</h1>;
+    return (
+      <Router>
+        <Route exact path="/">
+          <DestinationList/>
+        </Route>
+        <Route path="/destinations/create">
+          AddDestinationName component
+        </Route>
+        <Route path="/destinations/:DestinationId">
+          DestinationInfo component
+        </Route>
+        <Route path="/flights">
+          ViewFlights component
+        </Route>
+        <Route path="/flights/create">
+          AddFlightName component
+        </Route>
+        <Route path="/lodgings">
+          ViewLodgings component
+        </Route>
+        <Route path='/itineraries'>
+          viewItineraries
+        </Route>
+        <Route path="/itineraries/create">
+          AddItinerariesName
+        </Route>
+      </Router>
+    );
   }
 }
