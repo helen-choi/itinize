db6bcff27f256dbc7428c8dc7466a2493607b978
Helen Choi <helenchungg@gmail.com>
2020-05-07 20:07:19 +0000

    Stylized input

--- client/components/add-lodging-name.jsx
+++ client/components/add-lodging-name.jsx
@@ -18,9 +18,24 @@ export default class AddLodgingName extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <input type="text" name="lodgingName" value={this.state.lodgingName} onChange={this.handleChange}/>
-      </div>
+      <>
+        <div className="page-controls d-flex flex-nowrap">
+          <div className="col-4 mr-2 completed"></div>
+          <div className="col-4 mr-2 not-completed"></div>
+          <div className="col-4 not-completed"></div>
+        </div>
+        <div className="add-lodging-container p-3">
+          <div className="form-controls d-flex justify-content-between">
+            <i className="fas fa-times fa-2x"></i>
+            <i className="fas fa-arrow-right fa-2x"></i>
+          </div>
+          <h3 className="text-center pt-5">Add Lodging Name</h3>
+          <p className="text-muted text-center">Enter name of your lodge</p>
+          <div className="input-container row justify-content-center mt-5">
+            <input className="text-center p-2" type="text" name="lodgingName" placeholder="Lodge Name" value={this.state.lodgingName} onChange={this.handleChange} />
+          </div>
+        </div>
+      </>
     );
   }
 }
