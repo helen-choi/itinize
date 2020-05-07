fff16313b06fdb84cef28e1190a56e2f2144b08e
Helen Choi <helenchungg@gmail.com>
2020-05-07 18:32:53 +0000

    Value of input gets stored in state

--- client/components/add-lodging-name.jsx
+++ client/components/add-lodging-name.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
 export default class AddLodgingName extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: -1,
+      lodgingName: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    this.setState({
+      lodgingName: event.target.value
+    });
+  }
+
   render() {
     return (
-      <h1>Hello Gorgeous!</h1>
+      <div className="container">
+        <input type="text" name="lodgingName" value={this.state.lodgingName} onChange={this.handleChange}/>
+      </div>
     );
   }
 }
