abb3b7ca4f61a7240ec29091077c2599334cfd5d
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-06 18:15:22 -0700

    components/add-destination-name.jsx line 8-9 - added state component with componentStage property and destinationName property = to -1 and ''

--- client/components/add-destination-name.jsx
+++ client/components/add-destination-name.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 
 export default class AddDestinationName extends React.Component {
-  // constructor(props){
-  //   super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      componentStage: -1,
+      destinationName: ''
+    };
 
-  // }
+  }
+
   render() {
     return (
       <h1>hello</h1>
