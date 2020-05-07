473e58cf85465bb1bd60522627439a0b0006e9e6
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-06 21:42:03 -0700

    (1) add-destination-name.jsx - working on the progress bar of page, (2) changed src attribute value for index.html links, (3) added styling for icon

--- client/components/add-destination-name.jsx
+++ client/components/add-destination-name.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default class AddDestinationName extends React.Component {
   constructor(props) {
@@ -15,7 +15,26 @@ export default class AddDestinationName extends React.Component {
     // use either a switch or a two conditional check if -1 for each component to render correctly
     if (this.state.componentStage === -1) {
       return (
-        <h1>hello</h1>
+        <div className="container">
+          <header className="row">
+            <div className="row">
+              <div className="progress">
+                {/* <div className="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div> */}
+              </div>
+            </div>
+            <div className="row">
+              <Link className=""to="/">
+                <img className="" src="/./images/back.png" alt="back arrow"/>
+              </Link>
+            </div>
+            <div className="row">
+
+            </div>
+          </header>
+          <div className="row">
+            <h1>hello</h1>
+          </div>
+        </div>
       );
     } else {
       // either do a switch method or continue with this if-else statement
