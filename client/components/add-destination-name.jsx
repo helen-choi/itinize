eea2007a291ae4824fcc3471e316e0b01d7af529
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-06 22:54:16 -0700

    add-destination-name.jsx working on the aligning of the form input div

--- client/components/add-destination-name.jsx
+++ client/components/add-destination-name.jsx
@@ -20,16 +20,23 @@ export default class AddDestinationName extends React.Component {
             <div className="progress-bar w-25"></div>
           </div>
           <header className="row">
-            <div className="col">
+            <div className="col d-flex justify-content-between">
               <Link className="text-dark" to="/">
                 <i className="fas fa-arrow-left fa-2x"></i>
               </Link>
+              <Link className="text-dark" to="/">
+                <i className="fas fa-arrow-right fa-2x"></i>
+              </Link>
             </div>
           </header>
-          <div className="main-text-height row align-items-center">
+          <div className="main-text-height row align-items-center justify-content-center">
             <div className="col-12">
               <h4 className="text-center font-weight-bold">Add a destination</h4>
               <h6 className="text-center">Enter the country of your destination</h6>
+            </div>
+            {/* Fix this aligning to be correctly spaced */}
+            <div className="col-4 input-group">
+              <input type="text" className="form-control" placeholder="Country" name="" id=""/>
             </div>
           </div>
         </div>
