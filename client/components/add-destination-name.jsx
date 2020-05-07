1b3e7575889cbc8b2bc3b5c3a376927532be20bc
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-06 22:32:52 -0700

    add-destination-name.jsx - got positioning of main text to work by adding main-text-height class with height of 80% to div

--- client/components/add-destination-name.jsx
+++ client/components/add-destination-name.jsx
@@ -15,20 +15,22 @@ export default class AddDestinationName extends React.Component {
     // use either a switch or a two conditional check if -1 for each component to render correctly
     if (this.state.componentStage === -1) {
       return (
-        <div className="container">
+        <div className="container h-100">
           <div className="row progress mb-1">
             <div className="progress-bar w-25"></div>
           </div>
           <header className="row">
-            <Link className=""to="/">
-              <img className="icon-left" src="/./images/back.png" alt="back arrow"/>
-            </Link>
-            <div className="row">
-
+            <div className="col">
+              <Link className="text-dark" to="/">
+                <i className="fas fa-arrow-left fa-2x"></i>
+              </Link>
             </div>
           </header>
-          <div className="row">
-            <h4>Add a destination</h4>
+          <div className="main-text-height row align-items-center">
+            <div className="col-12">
+              <h4 className="text-center font-weight-bold">Add a destination</h4>
+              <h6 className="text-center">Enter the country of your destination</h6>
+            </div>
           </div>
         </div>
       );
