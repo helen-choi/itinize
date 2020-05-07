b9cd48ae9fd5cc64efad4e02b34d6fdb26228188
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-07 11:20:28 -0700

    component/add-destination-name.jsx - fixed form styling content alignment and took out link tag on right arrow. Also made a conditional rendering to prepare for changing of components using an array of react components and the component stage index

--- client/components/add-destination-name.jsx
+++ client/components/add-destination-name.jsx
@@ -13,36 +13,39 @@ export default class AddDestinationName extends React.Component {
 
   render() {
     // use either a switch or a two conditional check if -1 for each component to render correctly
-    if (this.state.componentStage === -1) {
-      return (
-        <div className="container h-100">
-          <div className="row progress mb-1">
-            <div className="progress-bar w-25"></div>
-          </div>
-          <header className="row">
-            <div className="col d-flex justify-content-between">
-              <Link className="text-dark" to="/">
-                <i className="fas fa-arrow-left fa-2x"></i>
-              </Link>
-              <Link className="text-dark" to="/">
-                <i className="fas fa-arrow-right fa-2x"></i>
-              </Link>
-            </div>
-          </header>
-          <div className="main-text-height row align-items-center justify-content-center">
-            <div className="col-12">
-              <h4 className="text-center font-weight-bold">Add a destination</h4>
-              <h6 className="text-center">Enter the country of your destination</h6>
-            </div>
-            {/* Fix this aligning to be correctly spaced */}
-            <div className="col-4 input-group">
-              <input type="text" className="form-control" placeholder="Country" name="" id=""/>
-            </div>
-          </div>
+    const componentsArray = [];
+    return (
+      <div className="container h-100">
+        <div className="row progress mb-1">
+          <div className="progress-bar w-25"></div>
         </div>
-      );
-    } else {
-      // either do a switch method or continue with this if-else statement
-    }
+        <header className="row">
+          <div className="col d-flex justify-content-between">
+            <Link className="text-dark" to="/">
+              <i className="fas fa-arrow-left fa-2x"></i>
+            </Link>
+            <i className="fas fa-arrow-right fa-2x"></i>
+            {/* history prop will be used at the end of the multi-page form */}
+          </div>
+        </header>
+        {(this.state.componentStage === -1 &&
+          <div className="main-text-height row align-items-center justify-content-center">
+            <div className="col">
+              <div className="row">
+                <div className="col">
+                  <h4 className="text-center font-weight-bold">Add a destination</h4>
+                  <h6 className="text-center">Enter the country of your destination</h6>
+                </div>
+              </div>
+              <div className="col input-group justify-content-center">
+                <input type="text" className="form-control" placeholder="Country" name="" id=""/>
+              </div>
+            </div>
+          </div>) || componentsArray[this.state.componentStage]}
+      </div>
+    );
+
+    // either do a switch method or continue with this if-else statement
+
   }
 }
