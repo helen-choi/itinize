b7706642787c270b04490a04994c00477872b344
frank <frankbae10@gmail.com>
2020-05-08 22:18:03 +0000

    deleted bunch of commented codes

--- client/components/flight-name.jsx
+++ client/components/flight-name.jsx
@@ -99,55 +99,5 @@ export default class AddFlightName extends React.Component {
         ) || pageArr[componentStage]}
       </div>
     );
-
-    // if (this.state.componentStage === -1) {
-    //   return (
-    // <div className="container">
-    //   <header className="row mt-4 px-1">
-    //     <div className="col d-flex justify-content-between">
-    //       <Link to='/' className="text-red">
-    //         <i className="fas fa-times fa-2x"></i>
-    //       </Link>
-    //       <i onClick={() => { this.handleNextClick(); }} className="fas fa-arrow-right fa-2x"></i>
-    //     </div>
-    //   </header>
-    //   <div className="row d-flex justify-content-center mt-5">
-    //     <h2>Name your flight</h2>
-    //   </div>
-    //   <div className="row d-flex justify-content-center mt-3">
-    //     <h6>Ex Returningflight, Going back home!</h6>
-    //   </div>
-    //   <div className="row d-flex justify-content-center mt-5">
-    //     <input type="text" onChange={this.handleChange.bind(this, 'flightName')} className="text-center flight-name" placeholder={'flight name'} value={this.state.flightName} />
-    //   </div>
-    // </div>;
-    //   );
-    // } else if (this.state.componentStage === 0) {
-    //   return (
-    //     <div className="container">
-    //       <header className="row mt-4 px-1">
-    //         <div className="col d-flex justify-content-between">
-    //           <i onClick={() => { this.handlePrevClick(); }} className="fas fa-arrow-left fa-2x"></i>
-    //           <i onClick={() => { this.handleNextClick(); }} className="fas fa-arrow-right fa-2x"></i>
-    //         </div>
-    //       </header>
-    // <div className="row d-flex justify-content-center text-center mt-5">
-    //   <h2>Enter your flight information</h2>
-    // </div>
-    // <div className="row d-flex justify-content-center text-center mt-3">
-    //   <h6>Ex LAX</h6>
-    // </div>
-    // <div className="row d-flex justify-content-center text-center mt-3">
-    //   <h6>Ex SW-1110</h6>
-    // </div>
-    // <div className="row d-flex justify-content-center mt-4">
-    //   <input type="text" onChange={this.handleChange.bind(this, 'airportDeparture')} className="text-center flight-number" placeholder={'Departure Airport Name'} value={this.state.airportDeparture} />
-    // </div>
-    // <div className="row d-flex justify-content-center mt-4">
-    //   <input type="text" onChange={this.handleChange.bind(this, 'flightNumber')} className="text-center flight-number" placeholder={'Flight Number'} value={this.state.flightNumber} />
-    // </div>
-    //     </div>
-    //   );
-    // }
   }
 }
