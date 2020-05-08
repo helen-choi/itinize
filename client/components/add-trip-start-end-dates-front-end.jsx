42cee372de2c795ab92eafd0952a8f106aa503d0
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-08 16:23:28 -0700

    client/components/add-trip-start-end-dates-front-end.jsx - formatted date tags to align well and will prepare for changing state of input

--- client/components/add-trip-start-end-dates-front-end.jsx
+++ client/components/add-trip-start-end-dates-front-end.jsx
@@ -10,11 +10,21 @@ export default class AddDestinationDates extends React.Component {
     return (
       <div className="row">
         <div className="col">
-          <h3 className="text-center pt-5">Select dates</h3>
-          <p className="text-muted text-center">Enter dates for start and end of your trip.</p>
-          <div className="input-container row justify-content-center mt-5">
-            <label htmlFor="tripStart"></label>
-            <input className="text-center p-2" type="date" name="tripStart" value={this.props.lodgingDates} onChange={this.props.handleChange} />
+          <div className="row">
+            <div className="col">
+              <h3 className="text-center pt-5">Select dates</h3>
+              <p className="text-muted text-center">Enter dates for start and end of your trip.</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-5 input-container justify-content-center mt-5">
+              <label className="d-block text-center" htmlFor="tripStart">Trip Start</label>
+              <input className="text-center p-2" type="date" name="tripStart" value={currentDate} onChange={this.props.handleChange} />
+            </div>
+            <div className="col-5 input-container justify-content-center mt-5">
+              <label className="d-block text-center" htmlFor="tripEnd">Trip End</label>
+              <input className="text-center p-2" type="date" name="tripStart" value={this.props.lodgingDates} onChange={this.props.handleChange} />
+            </div>
           </div>
         </div>
       </div>
