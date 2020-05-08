885e546fac0b683dfead796213fd1c073e3062ae
Helen Choi <helenchungg@gmail.com>
2020-05-08 00:08:20 +0000

    Appropriate status bar and icons rendering

--- client/components/add-lodging-conf-num.jsx
+++ client/components/add-lodging-conf-num.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class AddLodgingConfNum extends React.Component {
   render() {
     return (
-      <h1>Hello World!</h1>
+      <p>AddLodgingConfNum</p>
     );
   }
 }
--- client/components/add-lodging-name.jsx
+++ client/components/add-lodging-name.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import AddLodgingConfNum from './add-lodging-conf-num';
 
 export default class AddLodgingName extends React.Component {
   constructor(props) {
@@ -10,6 +11,8 @@ export default class AddLodgingName extends React.Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleNextClick = this.handleNextClick.bind(this);
+    this.handlePrevClick = this.handlePrevClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange() {
@@ -26,30 +29,94 @@ export default class AddLodgingName extends React.Component {
     });
   }
 
+  handlePrevClick() {
+    let counter = this.state.counter;
+    counter--;
+    this.setState({
+      counter: counter
+    });
+  }
+
+  handleSubmit() {
+
+  }
+
   render() {
-    // const counter = this.state.counter;
-    // let page;
-    // if(counter === 0)
+    const { counter } = this.state;
+    let stage = counter + 2;
+    let page;
+    const statusArr = [];
+    let leftIcon;
+    let rightIcon;
+
+    for (let i = 0; i < 3; i++) {
+      if (stage) {
+        statusArr.push('completed');
+        stage--;
+      } else {
+        statusArr.push('not-completed');
+      }
+    }
+
+    switch (counter) {
+      case -1:
+        page = (
+          <div className="add-lodging-name-container">
+            <h3 className="text-center pt-5">Add Lodging Name</h3>
+            <p className="text-muted text-center">Enter name of your lodge</p>
+            <div className="input-container row justify-content-center mt-5">
+              <input className="text-center p-2" type="text" name="lodgingName" placeholder="Lodge Name" value={this.state.lodgingName} onChange={this.handleChange} />
+            </div>
+          </div>
+        );
+        leftIcon = <Link to="/lodgings"><i className="fas fa-times fa-2x"></i></Link>;
+        rightIcon = <i className="fas fa-arrow-right fa-2x" onClick={this.handleNextClick}></i>;
+        break;
+      case 0:
+        page = <AddLodgingConfNum/>;
+        leftIcon = <i className="fas fa-arrow-left fa-2x" onClick={this.handlePrevClick}></i>;
+        rightIcon = <i className="fas fa-arrow-right fa-2x" onClick={this.handleNextClick}></i>;
+        break;
+      case 1:
+        page = 'AddLodgingDates';
+        leftIcon = <i className="fas fa-arrow-left fa-2x" onClick={this.handlePrevClick}></i>;
+        rightIcon = <i className="fas fa-check fa-2x" onClick={this.handleSubmit}></i>;
+        break;
+      case 2:
+        page = 'Confirmation';
+        break;
+      default:
+        page = (
+          <div className="add-lodging-name-container">
+            <h3 className="text-center pt-5">Add Lodging Name</h3>
+            <p className="text-muted text-center">Enter name of your lodge</p>
+            <div className="input-container row justify-content-center mt-5">
+              <input className="text-center p-2" type="text" name="lodgingName" placeholder="Lodge Name" value={this.state.lodgingName} onChange={this.handleChange} />
+            </div>
+          </div>
+        );
+    }
+
+    if (counter >= 0) {
+      const pageArr = [
+        <AddLodgingConfNum key={this.state.counter}/>, 'AddLodgingDates', 'Confirmation'
+      ];
+      page = pageArr[counter];
+    }
+
     return (
       <div className="add-lodging-container">
         <div className="page-controls d-flex flex-nowrap">
-          <div className="col-4 mr-2 completed"></div>
-          <div className="col-4 mr-2 not-completed"></div>
-          <div className="col-4 not-completed"></div>
+          <div className={`col-4 mr-2 ${statusArr[0]}`}></div>
+          <div className={`col-4 mr-2 ${statusArr[1]}`}></div>
+          <div className={`col-4 ${statusArr[2]}`}></div>
         </div>
-        <div className="add-lodging-name-container p-3">
-          <div className="form-controls d-flex justify-content-between">
-            <Link to="/lodgings">
-              <i className="fas fa-times fa-2x"></i>
-            </Link>
-            <i className="fas fa-arrow-right fa-2x" onClick={this.handleNextClick}></i>
-          </div>
-          <h3 className="text-center pt-5">Add Lodging Name</h3>
-          <p className="text-muted text-center">Enter name of your lodge</p>
-          <div className="input-container row justify-content-center mt-5">
-            <input className="text-center p-2" type="text" name="lodgingName" placeholder="Lodge Name" value={this.state.lodgingName} onChange={this.handleChange} />
-          </div>
+        <div className="form-controls d-flex justify-content-between p-3">
+
+          {leftIcon}
+          {rightIcon}
         </div>
+        {page}
       </div>
     );
   }
