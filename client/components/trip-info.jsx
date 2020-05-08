3ccba8f02b9db0c46d28c2346675677e8e84717a
frank <frankbae10@gmail.com>
2020-05-08 22:53:16 +0000

    added trip-info jsx file with a state set to view that holds flights property to an empty array

---
+++ client/components/trip-info.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class TripInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: { flights: [] }
+    };
+  }
+
+  render() {
+    return (
+      <h1>something</h1>
+    );
+  }
+}
