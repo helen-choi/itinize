66e58c2e665ec8c5be2ee325684dcc56ff4d1baf
Helen Choi <helenchungg@gmail.com>
2020-05-08 18:48:01 +0000

    Added state object

--- client/components/add-lodging-conf-number.jsx
+++ client/components/add-lodging-conf-number.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
 export default class AddLodgingConfNumber extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lodgingNumber: ''
+    };
+  }
+
   render() {
     return (
       <p>Hello Gorgeous!</p>
