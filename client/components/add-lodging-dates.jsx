679aa1df1324ebeb21676c36c2df8362f182fa01
Helen Choi <helenchungg@gmail.com>
2020-05-09 01:38:34 +0000

    Input styling

--- client/components/add-lodging-dates.jsx
+++ client/components/add-lodging-dates.jsx
@@ -43,15 +43,24 @@ export default class AddLodgingDates extends React.Component {
         <h3 className="text-center pt-5">Add Check-In/Out</h3>
         <p className="text-muted text-center">Save check-in and check-out<br />times to be notified</p>
         <div className="checkin row justify-content-center mt-5">
-          <label className="col-6" htmlFor="checkinDate">Checkin Date</label>
-          <label className="col-6" htmlFor="checkinTime">Time</label>
-          <input type="date" name="checkinDate" min={todayFormatted} max={this.state.checkoutTime} value={this.state.checkinDate} onChange={this.handleChange}/>
-          <input type="time" name="checkinTime" value={this.state.checkinTime} onChange={this.handleChange}/>
+          <div className="row flex-column col-6 mr-2">
+            <label className="" htmlFor="checkinDate">Check-In Date</label>
+            <input className="p-2 mr" type="date" id="checkinDate" name="checkinDate" min={todayFormatted} max={this.state.checkoutTime} value={this.state.checkinDate} onChange={this.handleChange} />
+          </div>
+          <div className="row flex-column col-4">
+            <label className="mr-2" htmlFor="checkinTime">Time</label>
+            <input className="p-2" type="time" id="checkinTime" name="checkinTime" value={this.state.checkinTime} onChange={this.handleChange} />
+          </div>
         </div>
         <div className="checkout row justify-content-center mt-5">
-          <label htmlFor="checkout-date"></label>
-          <input type="date" name="checkoutDate" min={checkOutMin} value={this.state.checkoutDate} onChange={this.handleChange} />
-          <input type="time" name="checkoutTime" value={this.state.checkoutTime} onChange={this.handleChange} />
+          <div className="row flex-column col-6 mr-2">
+            <label className="mr-2" htmlFor="checkoutDate">Check-Out Date</label>
+            <input className="p-2" type="date" id="checkoutDate" name="checkoutDate" min={checkOutMin} value={this.state.checkoutDate} onChange={this.handleChange} />
+          </div>
+          <div className="row flex-column col-4">
+            <label htmlFor="checkoutTime">Time</label>
+            <input className="p-2" type="time" id="checkoutTime" name="checkoutTime" value={this.state.checkoutTime} onChange={this.handleChange} />
+          </div>
         </div>
       </div>
     );
