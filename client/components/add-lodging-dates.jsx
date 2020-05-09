4118ff4599f29be1b8d83a62d61b69d2e4b94422
Helen Choi <helenchungg@gmail.com>
2020-05-09 00:40:20 +0000

    Added min and max values for date inputs

--- client/components/add-lodging-dates.jsx
+++ client/components/add-lodging-dates.jsx
@@ -35,7 +35,8 @@ export default class AddLodgingDates extends React.Component {
     const mm = String(today.getMonth() + 1).padStart(2, '0');
     const dd = String(today.getDate()).padStart(2, '0');
     const yyyy = today.getFullYear();
-    const todayFormated = `${yyyy}-${mm}-${dd}`;
+    const todayFormatted = `${yyyy}-${mm}-${dd}`;
+    const checkOutMin = this.state.checkinDate ? this.state.checkinDate : todayFormatted;
 
     return (
       <div className="add-lodging-number-container">
@@ -44,12 +45,12 @@ export default class AddLodgingDates extends React.Component {
         <div className="checkin row justify-content-center mt-5">
           <label className="col-6" htmlFor="checkinDate">Checkin Date</label>
           <label className="col-6" htmlFor="checkinTime">Time</label>
-          <input type="date" name="checkinDate" min={todayFormated} value={this.state.checkinDate} onChange={this.handleChange}/>
+          <input type="date" name="checkinDate" min={todayFormatted} max={this.state.checkoutTime} value={this.state.checkinDate} onChange={this.handleChange}/>
           <input type="time" name="checkinTime" value={this.state.checkinTime} onChange={this.handleChange}/>
         </div>
         <div className="checkout row justify-content-center mt-5">
           <label htmlFor="checkout-date"></label>
-          <input type="date" name="checkoutDate" min={todayFormated} value={this.state.checkoutDate} onChange={this.handleChange} />
+          <input type="date" name="checkoutDate" min={checkOutMin} value={this.state.checkoutDate} onChange={this.handleChange} />
           <input type="time" name="checkoutTime" value={this.state.checkoutTime} onChange={this.handleChange} />
         </div>
       </div>
