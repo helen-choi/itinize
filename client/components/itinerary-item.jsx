56466e47273baf613a4a2ba74ab559544d74860a
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-11 12:03:49 -0700

    implemented showing the feature by adding a display:none property that switches between each html tag that contains the note string

--- client/components/itinerary-item.jsx
+++ client/components/itinerary-item.jsx
@@ -8,18 +8,21 @@ export default class ListItineraryItem extends React.Component {
     };
   }
 
+  handleClick() {
+    this.setState({ isClicked: !this.state.isClicked });
+  }
+
   render() {
     let itineraryNote = this.props.itineraryNote;
     if (itineraryNote.length > 20) {
       itineraryNote = `${itineraryNote.slice(0, 20)}...`;
     }
     return (
-      <div data-toggle='collapse' data-target={`#${this.props.id}`} className="mt-1 border border-secondary col-9 ">
+      <div onClick={this.handleClick.bind(this)} data-toggle='collapse' data-target={`#${this.props.id}`} className="mt-1 border border-secondary col-9 ">
         <h3>{this.props.itineraryName}</h3>
         <p className="text-secondary">{this.props.itineraryDay}</p>
-        <p className="text-secondary">{itineraryNote}</p>
-        <p id={this.props.id} className="collapse text-secondary">{this.props.itineraryNote}</p>
-        <p id={this.props.id} className="d-none text-secondary">{this.props.itineraryNote}</p>
+        <p className={`${(this.state.isClicked) ? 'd-none' : null} text-secondary`}>{itineraryNote}</p>
+        <p id={this.props.id} className={`${(this.state.isClicked) ? '' : 'd-none'} text-secondary itinerary-display`}>{this.props.itineraryNote}</p>
       </div>
     );
   }
