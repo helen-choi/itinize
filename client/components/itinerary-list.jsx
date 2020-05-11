aab87674ee6e59753cdf24fa4cad46cdb109b7e0
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-10 23:02:15 -0700

    client/components/itinerary-list.jsx - completed itinerary list item mockup

--- client/components/itinerary-list.jsx
+++ client/components/itinerary-list.jsx
@@ -4,7 +4,7 @@ export default class ItineraryList extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
+        <div className="mt-2 row">
           <div className="col-6">
             <i className="far fa-times-circle fa-2x"></i>
           </div>
@@ -14,7 +14,7 @@ export default class ItineraryList extends React.Component {
             <i className="far ml-2 fa-compass fa-2x text-black"></i>
           </div>
         </div>
-        <div className="row">
+        <div className="mt-2 row">
           <div className="col">
             <h1>{this.props.location.state.destinationName}</h1>
           </div>
@@ -22,19 +22,34 @@ export default class ItineraryList extends React.Component {
         <div className="row">
           {/* pass days via props to see how many tags to render */}
           <div className="col">
-            <button type="button" className='btn btn-sm btn-outline-primary'>All</button>
+            <button type="button" className='mr-1 btn btn-sm btn-outline-primary'>All</button>
             <button type="button" className='btn btn-sm btn-outline-secondary'>Day One</button>
             <button type="button" className='btn btn-sm btn-outline-danger'>Day Two</button>
             <button type="button" className='btn btn-sm btn-outline-success'>All</button>
           </div>
         </div>
         {/* list items mockup below */}
-        <div className="row">
-
-          <div className="row"></div>
-          <div className="row"></div>
-          <div className="row"></div>
-          <div className="row"></div>
+        <div className="mt-2 row justify-content-center">
+          <div className="mt-1 border border-secondary col-9">
+            <h3>Tiger Sugar</h3>
+            <p className="text-secondary">Day 2</p>
+            <p className="text-secondary">I want to do thissssssss</p>
+          </div>
+          <div className="mt-1 border border-secondary col-9">
+            <h3>Tiger Sugar</h3>
+            <p className="text-secondary">Day 2</p>
+            <p className="text-secondary">I want to do thissssssss</p>
+          </div>
+          <div className="mt-1 border border-secondary col-9">
+            <h3>Tiger Sugar</h3>
+            <p className="text-secondary">Day 2</p>
+            <p className="text-secondary">I want to do thissssssss</p>
+          </div>
+          <div className="mt-1 border border-secondary col-9">
+            <h3>Tiger Sugar</h3>
+            <p className="text-secondary">Day 2</p>
+            <p className="text-secondary">I want to do thissssssss</p>
+          </div>
         </div>
       </div>
     );
