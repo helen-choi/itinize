17736d1661f13803ee0cfa0c18c7b531ac6cf1c7
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-10 20:11:13 -0700

    client/components/itinerary-list.jsx - constructed initial format bootstrap rows for view

--- client/components/itinerary-list.jsx
+++ client/components/itinerary-list.jsx
@@ -3,7 +3,35 @@ import React from 'react';
 export default class ItineraryList extends React.Component {
   render() {
     return (
-      <h1>Hello!</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            x icon
+          </div>
+          <div className="col-6">
+            pencil
+            +
+            gps
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            title
+          </div>
+        </div>
+        <div className="row">
+          {/* pass days via props to see how many tags to render */}
+          <div className="col">tags</div>
+        </div>
+        {/* list items mockup below */}
+        <div className="row">
+
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+        </div>
+      </div>
     );
   }
 }
