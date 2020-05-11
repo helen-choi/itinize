e186f87cc8d81cf152ea8ef81db587fdefa4fb88
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-11 10:59:51 -0700

    adjusted itinerary item component to render conditional elipses if note has more than 20 text characters

--- client/components/itinerary-item.jsx
+++ client/components/itinerary-item.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 export default function ListItineraryItem(props) {
+  let itineraryNote = props.itineraryNote;
+  if (itineraryNote.length > 20) {
+    itineraryNote = `${itineraryNote.slice(0, 20)}...`;
+  }
   return (
     <div className="mt-1 border border-secondary col-9">
       <h3>{props.itineraryName}</h3>
       <p className="text-secondary">{props.itineraryDay}</p>
-      <p className="text-secondary">{`${props.itineraryNote.slice(0, 20)}...`}</p>
+      <p className="text-secondary">{itineraryNote}</p>
     </div>
   );
 }
