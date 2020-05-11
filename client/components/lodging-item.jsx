0c23beb1836c64429d760aef534688b1118f87b2
Helen Choi <helenchungg@gmail.com>
2020-05-11 20:51:36 +0000

    handleDelete method gets triggered

--- client/components/lodging-item.jsx
+++ client/components/lodging-item.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 
-export default function LodgingItem(props) {
-  const lodging = props.lodging;
+export default class LodgingItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
 
-  return (
-    <div className="lodging p-4 mt-3 position-relative">
-      <div className="delete-control text-right position-absolute">
-        <i className="fas fa-times"></i>
+  handleDelete() {
+    // eslint-disable-next-line no-console
+    console.log('item deleted');
+  }
+
+  render() {
+    const lodging = this.props.lodging;
+    return (
+      <div className="lodging p-4 mt-3 position-relative">
+        <div className="delete-control text-right position-absolute" >
+          <i className="fas fa-times" onClick={this.handleDelete}></i>
+        </div>
+        <h5>{lodging.lodgingName}</h5>
+        <p><strong>Confirmation: </strong>{lodging.lodgingConfNum}</p>
+        <p><strong>Check-In:</strong> {lodging.checkInDateTime}</p>
+        <p><strong>Check-Out:</strong> {lodging.checkOutDateTime}</p>
       </div>
-      <h5>{lodging.lodgingName}</h5>
-      <p><strong>Confirmation: </strong>{lodging.lodgingConfNum}</p>
-      <p><strong>Check-In:</strong> {lodging.checkInDateTime}</p>
-      <p><strong>Check-Out:</strong> {lodging.checkOutDateTime}</p>
-    </div>
-  );
+    );
+  }
 }
