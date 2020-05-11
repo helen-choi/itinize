ba87ccea9439c6a9a7f2fac6609302c08c831b53
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-10 20:49:37 -0700

    client/components/itinerary-list.jsx - took note of how to pass the destintation name into the component via the state property of React Router

--- client/components/destination-info.jsx
+++ client/components/destination-info.jsx
@@ -183,6 +183,7 @@ export default class DestinationInfo extends React.Component {
                   </Link>
                 </div>
                 <div className="col-3">
+                  {/* prepare to pass destinationName via the state property in the Link component! */}
                   <Link to={{ pathname: '/itineraries', state: { destinationId: destinationId } }} className="circle teal m-auto d-flex justify-content-center align-items-center">
                     <i className="fas fa-map-marker-alt fa-lg"></i>
                   </Link>
