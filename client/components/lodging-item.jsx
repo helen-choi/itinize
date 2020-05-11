1bf536babb36bc7f950e3f5675922806737d051d
Helen Choi <helenchungg@gmail.com>
2020-05-11 20:33:21 +0000

    Added delete icons on each lodging item

--- client/components/lodging-item.jsx
+++ client/components/lodging-item.jsx
@@ -4,7 +4,10 @@ export default function LodgingItem(props) {
   const lodging = props.lodging;
 
   return (
-    <div className="lodging p-4 mt-3">
+    <div className="lodging p-4 mt-3 position-relative">
+      <div className="delete-control text-right position-absolute">
+        <i className="fas fa-times"></i>
+      </div>
       <h5>{lodging.lodgingName}</h5>
       <p><strong>Confirmation: </strong>{lodging.lodgingConfNum}</p>
       <p><strong>Check-In:</strong> {lodging.checkInDateTime}</p>
