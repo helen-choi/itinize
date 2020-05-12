3821010ca1e85cbf6a0290224459bf75d1362a60
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-11 21:20:30 -0700

    implemented React Bootstrap modal to delete destination based on Id by passing down method and id via props

--- client/components/delete-modal.jsx
+++ client/components/delete-modal.jsx
@@ -2,30 +2,31 @@ import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 
-export default function DeleteModal() {
+export default function DeleteModal(props) {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
     <>
-      <Button variant="light" onClick={handleShow}>
-        Delete
-      </Button>
-
+      <div className="circle red m-auto d-flex justify-content-center align-items-center">
+        <i onClick={handleShow} handler="delete" className="fas fa-trash-alt fa-lg"></i>
+      </div>
+      {/* this.handleClickDelete(destinationInfo.destinationId) */}
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+        <Modal.Header>
+          <Modal.Title>Warning</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>Are you sure you want to delete this item?</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
           <Button variant="danger" onClick={() => {
             handleClose();
+            props.handleDelete(props.destinationId);
           }}>
-            Save Changes
+            Delete
           </Button>
         </Modal.Footer>
       </Modal>
