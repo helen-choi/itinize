22a83c5d69facda615076c1aef040e8b209f5af3
kevin-tolentino <kevin.tolentino95@gmail.com>
2020-05-12 12:34:17 -0700

    implemented vertically centered DeleteModal component to render

--- client/components/destination-info.jsx
+++ client/components/destination-info.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SelectDestinationImageProfile from './select-destination-image-profile';
+import DeleteModal from './delete-modal';
 
 export default class DestinationInfo extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ export default class DestinationInfo extends React.Component {
     this.handleUserInputOnChange = this.handleUserInputOnChange.bind(this);
     this.handleExitEditImage = this.handleExitEditImage.bind(this);
     this.handleEditImage = this.handleEditImage.bind(this);
+    this.handleClickDelete = this.handleClickDelete.bind(this);
     this.state = {
       destinationInfo: null,
       destinationName: '',
@@ -192,7 +194,6 @@ export default class DestinationInfo extends React.Component {
                   </Link>
                 </div>
                 <div className="col-3">
-                  {/* prepare to pass destinationName via the state property in the Link component! */}
                   <Link to={{
                     pathname: '/itineraries',
                     state: {
@@ -207,9 +208,7 @@ export default class DestinationInfo extends React.Component {
                   </Link>
                 </div>
                 <div className="col-3">
-                  <div className="circle red m-auto d-flex justify-content-center align-items-center">
-                    <i onClick={() => this.handleClickDelete(destinationInfo.destinationId)} handler="delete" className="fas fa-trash-alt fa-lg"></i>
-                  </div>
+                  <DeleteModal destinationId={destinationId} handleDelete={this.handleClickDelete}/>
                 </div>
               </footer>
             </>
