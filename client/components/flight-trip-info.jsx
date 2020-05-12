3eae59a44c701b207a75c607c3039c0ab208be89
frank <frankbae10@gmail.com>
2020-05-12 19:07:11 +0000

    changed styling to be uniformed as lodging

--- client/components/flight-trip-info.jsx
+++ client/components/flight-trip-info.jsx
@@ -62,29 +62,33 @@ export default class TripInfo extends React.Component {
       <div className="container">
         <header className="row justify-content-between pt-2 flex-fill mt-2">
           <Link to={`/destinations/${this.props.location.state.destinationId}`} className="col-2">
-            <i className="fas fa-arrow-left fa-2x text-black"></i>
+            <i className="fas fa-times fa-2x text-black"></i>
           </Link>
           <div onClick={this.deleteIconEdit} className="col-2">
-            <i className="fas fa-trash fa-2x text-black"></i>
+            <i className="fas fa-pen fa-2x text-black"></i>
           </div>
         </header>
         <div className="row justify-content-center mt-4">
           <h2>{this.props.location.state.destinationName}</h2>
         </div>
-        <div>
+        <div className="pl-3 pr-3 mt-3">
           {
             this.state.flights.map(flight => {
               return <FlightTripInfoItem toggle={this.toggleDeleteEdit} handleClickDelete={this.handleClickDelete} key={flight.flightId} flightData={flight} />;
             })
           }
         </div>
-        <div className="flight-create">
-          <Link to={{
-            pathname: '/flights/create',
-            state: { destinationId: this.props.location.state.destinationId }
-          }} className="m-auto d-flex justify-content-center align-items-center">
-            <i className="fas fa-plus fa-4x pt-3 text-muted"></i>
-          </Link>
+        <div className="pl-3 pr-3 mt-3">
+          <div className="gray-box p-4 d-flex justify-content-center">
+            <Link to={{
+              pathname: '/flights/create',
+              state: { destinationId: this.props.location.state.destinationId }
+            }} className="m-auto d-flex justify-content-center align-items-center">
+              <div className="add-lodging-item d-flex justify-content-center align-items-center">
+                <i className="fas fa-plus fa-2x"></i>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     );
