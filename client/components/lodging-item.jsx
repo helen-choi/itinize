8d19494c9a272ec01dff06fb973a3dee1c750bf8
Helen Choi <helenchungg@gmail.com>
2020-05-13 01:15:36 +0000

    Added window requestAnimationFrame function to animate the swiping

--- client/components/lodging-item.jsx
+++ client/components/lodging-item.jsx
@@ -12,11 +12,11 @@ export default class LodgingItem extends React.Component {
 
     this.onDragStartMouse = this.onDragStartMouse.bind(this);
     this.onDragStart = this.onDragStart.bind(this);
-    // this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
     // this.onDragEndMouse = this.onDragEndMouse.bind(this);
     // this.onDragEnd = this.onDragEnd.bind(this);
     // this.onSwiped = this.onSwiped.bind(this);
-    // this.updatePosition = this.updatePosition.bind(this);
+    this.updatePosition = this.updatePosition.bind(this);
 
     this.handleDelete = this.handleDelete.bind(this);
   }
@@ -28,13 +28,35 @@ export default class LodgingItem extends React.Component {
 
   onDragStartMouse(event) {
     this.onDragStart(event.clientX);
-    // window.addEventListener('mousemove', this.onMouseMove);
+    window.addEventListener('mousemove', this.onMouseMove);
   }
 
   onDragStart(clientX) {
     this.dragged = true;
     this.dragStartX = clientX;
-    // window.requestAnimationFrame(this.updatePosition);
+    window.requestAnimationFrame(this.updatePosition);
+  }
+
+  updatePosition() {
+    if (this.dragged) requestAnimationFrame(this.updatePosition);
+
+    this.list.style.transform = `translateX(${this.left}px)`;
+
+    const opacity = (Math.abs(this.left) / 100).toFixed(2);
+    if (opacity < 1 && opacity.toString() !== this.background.style.opacity) {
+      this.background.style.opacity = opacity.toString();
+    }
+    if (opacity >= 1) {
+      this.background.style.opacity = '1';
+    }
+
+  }
+
+  onMouseMove(event) {
+    const left = event.clientX - this.dragStartX;
+    if (left < 0) {
+      this.left = left;
+    }
   }
 
   render() {
@@ -42,7 +64,10 @@ export default class LodgingItem extends React.Component {
     const lodging = this.props.lodging;
     return (
       <div className="wrapper" ref={div => (this.wrapper = div)}>
-        <div className="lodging p-4 mt-3 position-relative" ref={div => (this.list = div)} onMouseDown={this.onDragStartMouse}>
+        <div className="background" ref={div => (this.background = div)}></div>
+        <div className="lodging p-4 mt-3 position-relative"
+          ref={div => (this.list = div)}
+          onMouseDown={this.onDragStartMouse}>
           <div className="delete-control text-right position-absolute" style={modalStyle} >
             <i className="fas fa-times" onClick={this.handleDelete}></i>
           </div>
@@ -51,7 +76,6 @@ export default class LodgingItem extends React.Component {
           <p><strong>Check-In:</strong> {lodging.checkInDateTime}</p>
           <p><strong>Check-Out:</strong> {lodging.checkOutDateTime}</p>
         </div>
-        <div className="background" ref={div => (this.background = div)}></div>
       </div>
     );
   }
