e080b821b3343f044922e23b596bc62dd647a962
frank <frankbae10@gmail.com>
2020-05-13 02:55:15 +0000

    able to get flight current date and user input dates

--- client/components/flight-trip-info-item.jsx
+++ client/components/flight-trip-info-item.jsx
@@ -11,6 +11,7 @@ export default class FlightTripInfoItem extends React.Component {
       departTime: '',
       arrivalTime: '',
       arrivalDay: '',
+      departureDate: '',
       departingDate: false
     };
   }
@@ -18,33 +19,68 @@ export default class FlightTripInfoItem extends React.Component {
   componentDidMount() {
     this.setState({
       flightNumber: this.props.flightData.flightNumber
-    }, () => this.getFlightStatus());
+    });
+    this.checkDate(this.props.flightData.flightDate.slice(0, 10));
   }
 
-  getFlightStatus() {
-    const key = 'a75149fdc4b4fb2bc75f00a6c9659e91';
-    const flightIata = this.state.flightNumber;
-    const departureIata = this.props.flightData.airportDeparture;
-    fetch(`http://api.aviationstack.com/v1/flights?access_key=${key}&flight_iata=${flightIata}&dep_iata=${departureIata}`)
-      .then(res => res.json())
-      .then(data => {
-        if (data.data.length !== 0) {
-          const flightUpdate = data.data.length - 1;
-          this.setState({
-            flightStatus: data.data[flightUpdate].flight_status,
-            airportArrival: data.data[flightUpdate].arrival.iata,
-            departTime: data.data[flightUpdate].departure.scheduled.slice(11, 16),
-            arrivalTime: data.data[flightUpdate].arrival.scheduled.slice(11, 16),
-            arrivalDay: data.data[flightUpdate].arrival.scheduled.slice(0, 10),
-            departingDate: true
-          });
-        }
-      })
-      .catch(err => console.error(err));
+  checkDate(date) {
+    console.log(date);
+    const userYear = date.toString().slice(0, 4);
+    const userMonth = date.toString().slice(5, 7);
+    const userDay = date.toString().slice(8, 10);
+    const currentDate = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+
+    console.log(userYear);
+    console.log(userMonth);
+    console.log(userDay);
+    console.log(currentDate);
+
+    // if (userDate === currentDate) {
+    //   console.log('hello its me');
+    // } else {
+    //   console.log('this is not me');
+
+    // }
   }
+
+  // if the date is correct should call the getFlightStatus method
+  // if not, then put pending
+
+  //  () => this.getFlightStatus()
+
+  // getFlightStatus() {
+  //   // const key = 'a75149fdc4b4fb2bc75f00a6c9659e91';
+  //   const flightIata = this.state.flightNumber;
+  //   const departureIata = this.props.flightData.airportDeparture;
+  //   fetch(`http://api.aviationstack.com/v1/flights?access_key=${key}&flight_iata=${flightIata}&dep_iata=${departureIata}`)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       if (data.data.length !== 0) {
+  //         const flightUpdate = data.data.length - 1;
+  //         this.setState({
+  //           flightStatus: data.data[flightUpdate].flight_status,
+  //           airportArrival: data.data[flightUpdate].arrival.iata,
+  //           departTime: data.data[flightUpdate].departure.scheduled.slice(11, 16),
+  //           arrivalTime: data.data[flightUpdate].arrival.scheduled.slice(11, 16),
+  //           arrivalDay: data.data[flightUpdate].arrival.scheduled.slice(0, 10),
+  //           departingDate: true
+  //         });
+  //       }
+  //     })
+  //     .catch(err => console.error(err));
+  // }
 
   render() {
     const flightData = this.props.flightData;
+
+    // const userInput = new Date(2020, 4, 12);
+    // const a = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+    // console.log(a);
+    // if (userInput.toString() === a.toString()) {
+    //   console.log('it worked');
+    // } else {
+    //   console.log('didnt work');
+    // }
 
     const flightStatus = (this.state.departingDate) ? this.state.flightStatus : 'pending';
     const airportArrival = (this.state.departingDate) ? this.state.airportArrival : 'pending';
