aa4895adbd2f4b66740bf6bd2be652fea0256bb1
Helen Choi <helenchungg@gmail.com>
2020-05-18 06:07:05 +0000

    lodging form responsive

--- client/components/add-lodging-conf-number.jsx
+++ client/components/add-lodging-conf-number.jsx
@@ -6,7 +6,7 @@ export default class AddLodgingConfNumber extends React.Component {
     return (
       <div className="add-lodging-number-container">
         <h3 className="text-center pt-5">Add Lodging<br/>Confirmation Number</h3>
-        <div className="input-container row justify-content-center mt-5">
+        <div className="input-container row justify-content-center mt-5 w-100">
           <input className="text-center p-2" type="text" name="lodgingNumber" placeholder="Confirmation Number" value={this.props.value} onChange={this.props.handleChange} />
         </div>
       </div>
